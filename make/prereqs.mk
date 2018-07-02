60c4a35e4514589e7170628de2a0a19570cde3fd
ginkgoMZD <mzd@ginkgostreet.com>
2018-07-02 11:47:18 -0400

    add repo.certbot; misc clean-up;

--- make/prereqs.mk
+++ make/prereqs.mk
@@ -2,16 +2,21 @@
 this-dir := $(dir $(lastword $(MAKEFILE_LIST)))
 include $(this-dir)/inc.functions.mk
 
-all: wget git
+all: wget git debconf-utils repo.certbot
 
 .PHONY: wget
 wget:
-	@ $(call install-package, wget)
+	$(call install-package, wget)
 
 .PHONY: git
 git:
-	@ $(call install-package, git)
+	$(call install-package, git)
 
 .PHONY: debconf-utils
 debconf-utils:
-	@ $(call install-package, debconf-utils)
+	$(call install-package, debconf-utils)
+
+repo.certbot:
+	# Add the repo for CertBot/Let's Encrypt
+	sudo add-apt-repository ppa:certbot/certbot
+	touch repo.certbot
