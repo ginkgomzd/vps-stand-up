6d3f0188e6ebec55f7343f0a91d640ee22bee385
ginkgoMZD <mzd@ginkgostreet.com>
2018-07-02 18:02:51 -0400

    fix bugs

--- make/composer.mk
+++ make/composer.mk
@@ -1,22 +1,24 @@
 
 CHECK_SHA384 := $(shell wget -q https://composer.github.io/installer.sig -O -)
-INSTALL_DIR := '/usr/local/share/composer'
-INSTALL_BIN := '/usr/local/bin/composer'
+INSTALL_PATH := '/usr/local/bin'
 
+all: install clean-up
+
+.PHONY: install
 install: verify install-composer.php
-	php install-composer.php --install-dir=$(INSTALL_DIR) --filename=composer
-	ln -s $(INSTALL_DIR)/composer $(INSTALL_BIN)
+	php install-composer.php --install-dir=$(INSTALL_PATH) --filename=composer
+
+install-composer.php:
+		wget -q 'https://getcomposer.org/installer' -O 'install-composer.php'
 
 check.sha384:
 	echo $(CHECK_SHA384) install-composer.php > check.sha384
-
-install-composer.php:
-	wget -q 'https://getcomposer.org/installer' -O 'install-composer.php'
 
 .PHONY: verify
 verify: check.sha384 install-composer.php
 	sha384sum -c check.sha384
 
-clean:
+.PHONY: clean-up
+clean-up:
 	rm check.sha384
 	rm install-composer.php
