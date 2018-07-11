3feb9ea6e304518f18ed75f134d96540e73470c8
ginkgoMZD <mzd@ginkgostreet.com>
2018-07-11 11:46:27 -0400

    more robust, more concise

--- make/sys-utils.mk
+++ make/sys-utils.mk
@@ -2,28 +2,34 @@
 this-dir := $(dir $(lastword $(MAKEFILE_LIST)))
 include $(this-dir)/inc.functions.mk
 
-all: sys-utils/keyscan sys-utils/linode-etc sys-utils/backup-mysql sys-utils/env-utils sys-utils/dkim-tools /usr/local/sbin/mysqltuner
+all: sys-utils sys-utils/keyscan sys-utils/linode-etc sys-utils/backup-mysql sys-utils/env-utils sys-utils/dkim-tools /usr/local/sbin/mysqltuner
+
+sys-utils:
+	mkdir $(@)
 
 sys-utils/keyscan:
 	test -d ~/.ssh || mkdir ~/.ssh
 	ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 	ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts
-	touch sys-utils/keyscan
+	touch $(@)
 
-linode-etc: sys-utils/keyscan
-	git clone git@bitbucket.org:ginkgostreet/linode-etc.git sys-utils/linode-etc
+sys-utils/linode-etc: sys-utils/keyscan
+	- rm -r $(@)
+	git clone git@bitbucket.org:ginkgostreet/linode-etc.git $(@)
 
 sys-utils/backup-mysql: sys-utils/keyscan
-	git clone git@bitbucket.org:ginkgostreet/backup-mysql.git sys-utils/backup-mysql
+	- rm -r $(@)
+	git clone git@bitbucket.org:ginkgostreet/backup-mysql.git $(@)
 
 sys-utils/env-utils: sys-utils/keyscan
-	git clone git@bitbucket.org:ginkgostreet/env-utils-v3.git sys-utils/env-utils
+	- rm -r $(@)
+	git clone git@bitbucket.org:ginkgostreet/env-utils-v3.git $(@)
 	cd sys-utils/env-utils && \
 	git checkout linode-1.0
 
 sys-utils/dkim-tools:
 	$(call install-package, opendkim-tools)
-	@ touch sys-utils/dkim-tools
+	@ touch $(@)
 
 /usr/local/sbin/mysqltuner:
 	wget "http://mysqltuner.pl" -O /usr/local/sbin/mysqltuner
@@ -32,4 +38,3 @@ sys-utils/dkim-tools:
 .PHONY: sys-utils.clean
 sys-utils.clean:
 	rm -rf sys-utils/
-	rm -f sys-utils/keyscan
