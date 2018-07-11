5ca4a6e4d95d4a86313cb6a453fdaa07b0b1158b
ginkgoMZD <mzd@ginkgostreet.com>
2018-07-11 10:38:03 -0400

    clean up a mess

--- make/security.mk
+++ make/security.mk
@@ -2,13 +2,14 @@
 this-dir := $(dir $(lastword $(MAKEFILE_LIST)))
 include $(this-dir)/inc.functions.mk
 
-all: security.fail2ban security.rkhunter
+all: security.fail2ban security.bad-bot-blocker security.rkhunter
 
 security.fail2ban:
 	$(call install-package, fail2ban)
 	@ touch security.fail2ban
 
-security.bad-bot-blocker: sys-utils.keyscan
+# TODO: install bad-bot-blocker into sys-utils?? pros? cons?
+security.bad-bot-blocker:
 	git clone git@github.com:ginkgostreet/bad-bot-blocker.git sys-utils/bad-bot-blocker
 
 security.rkhunter:
--- make/sys-utils.mk
+++ make/sys-utils.mk
@@ -2,21 +2,21 @@
 this-dir := $(dir $(lastword $(MAKEFILE_LIST)))
 include $(this-dir)/inc.functions.mk
 
-all: sys-utils/linode-etc sys-utils/backup-mysql sys-utils/env-utils sys-utils/bad-bot-blocker sys-utils/rkhunter
+all: sys-utils/keyscan sys-utils/linode-etc sys-utils/backup-mysql sys-utils/env-utils sys-utils/dkim-tools /usr/local/sbin/mysqltuner
 
-sys-utils.keyscan:
+sys-utils/keyscan:
 	test -d ~/.ssh || mkdir ~/.ssh
 	ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 	ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts
-	touch sys-utils.keyscan
+	touch sys-utils/keyscan
 
-sys-utils/linode-etc: sys-utils.keyscan
+sys-utils/linode-etc: sys-utils/keyscan
 	git clone git@bitbucket.org:ginkgostreet/linode-etc.git sys-utils/linode-etc
 
-sys-utils/backup-mysql: sys-utils.keyscan
+sys-utils/backup-mysql: sys-utils/keyscan
 	git clone git@bitbucket.org:ginkgostreet/backup-mysql.git sys-utils/backup-mysql
 
-sys-utils/env-utils: sys-utils.keyscan
+sys-utils/env-utils: sys-utils/keyscan
 	git clone git@bitbucket.org:ginkgostreet/env-utils-v3.git sys-utils/env-utils
 	cd sys-utils/env-utils && \
 	git checkout linode-1.0
@@ -32,4 +32,4 @@ sys-utils/dkim-tools:
 .PHONY: sys-utils.clean
 sys-utils.clean:
 	rm -rf sys-utils/
-	rm -f sys-utils.keyscan
+	rm -f sys-utils/keyscan
