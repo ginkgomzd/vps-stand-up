86a624eddf6c65b8a122b8d29fc8a17fb6d4ced3
ginkgoMZD <mzd@ginkgostreet.com>
2018-07-11 14:11:05 -0400

    fixes

--- make/rkhunter.mk
+++ make/rkhunter.mk
@@ -1,15 +1,20 @@
 
+RKHUNTER_VER := 1.4.6
+
 /usr/local/bin/rkhunter: /usr/local/share/rkhunter
-	cd /usr/local/share && \
-	./installer.sh --install
+	cd /usr/local/share/rkhunter && \
+	./installer.sh --install > /dev/null
 
 /usr/local/share/rkhunter:
 	cd /usr/local/share && \
-	wget https://sourceforge.net/projects/rkhunter/files/rkhunter/1.4.6/rkhunter-1.4.6.tar.gz -O rkhunter.tar.gz
-	tar xzf ../rkhunter-1.4.6.tar.gz --xform="s/rkhunter-1.4.6/rkhunter/"
+	wget https://sourceforge.net/projects/rkhunter/files/rkhunter/$(RKHUNTER_VER)/rkhunter-$(RKHUNTER_VER).tar.gz -O rkhunter.tar.gz
+	cd /usr/local/share && \
+	tar xzf rkhunter.tar.gz --xform="s/rkhunter-$(RKHUNTER_VER)/rkhunter/"
+	rm /usr/local/share/rkhunter.tar.gz
 
+.PHONY: clean
 clean:
-	rm -f rkhunter.tar.gz
+	rm -rf /usr/local/share/rkhunter /usr/local/bin/rkhunter
 
 # TODO:
 # don't think the following should have been part of the install:
