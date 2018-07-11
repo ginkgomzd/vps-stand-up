5e5d390c0ed5be93e0ad88025f1e5e0e10a6491d
ginkgoMZD <mzd@ginkgostreet.com>
2018-07-11 16:18:36 -0400

    convert open-files-limit to make

--- make/conf.server.mk
+++ make/conf.server.mk
@@ -1,9 +1,20 @@
 
 this-dir := $(dir $(lastword $(MAKEFILE_LIST)))
 
+REPLACE_CMD := $(this-dir)../bin/replace_file
+PATCH_FILE_CMD := $(this-dir)../bin/patch_file
+
 conf.server.mysql:
 	$(MAKE) -f $(this-dir)conf.server.mysql.mk
-	$(this-dir)../bin/055-open-files-limit
+	# Open-file Limits:
+	$(PATCH_FILE_CMD) pam.d/common-session
+	$(PATCH_FILE_CMD) pam.d/common-session-noninteractive
+	$(PATCH_FILE_CMD) /usr/sbin/pam-auth-update
+	$(REPLACE_CMD) security/limits.d/01-mysql.conf
+	$(REPLACE_CMD) sysctl.d/20-open-files.conf
+	$(REPLACE_CMD) systemd/system/mysql.service.d/override.conf
+	systemctl daemon-reload
+	# End Open-File Limits;
 	service mysql restart
 	@ touch $(@)
 
