a166a11dced8a7971685ff14eff2e0b0f0150dbb
ginkgoMZD <mzd@ginkgostreet.com>
2018-07-11 14:16:14 -0400

    fixes

--- make/security.mk
+++ make/security.mk
@@ -2,16 +2,17 @@
 this-dir := $(dir $(lastword $(MAKEFILE_LIST)))
 include $(this-dir)/inc.functions.mk
 
-all: security.fail2ban security.bad-bot-blocker security.rkhunter
+all: security.fail2ban sys-utils/bad-bot-blocker security.rkhunter
 
 security.fail2ban:
 	$(call install-package, fail2ban)
-	@ touch security.fail2ban
+	@ touch $(@)
 
 # TODO: install bad-bot-blocker into sys-utils?? pros? cons?
-security.bad-bot-blocker:
-	git clone git@github.com:ginkgostreet/bad-bot-blocker.git sys-utils/bad-bot-blocker
+sys-utils/bad-bot-blocker:
+	- rm -rf $(@)
+	git clone git@github.com:ginkgostreet/bad-bot-blocker.git $(@)
 
 security.rkhunter:
 	$(MAKE) -f $(this-dir)/rkhunter.mk
-	@ touch security.rkhunter
+	@ touch $(@)
