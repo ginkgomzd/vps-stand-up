0a58a058e6028059cd63033c1214209cf4941c2d
ginkgoMZD <mzd@ginkgostreet.com>
2020-12-08 12:08:57 -0500

    uninstall node and install LTS version;

--- make/nodejs.mk
+++ make/nodejs.mk
@@ -1,12 +1,19 @@
 
 default: install
 
+# setup for node 10.x LTS
 node-source-setup.sh:
-	curl -sL https://deb.nodesource.com/setup_11.x > $@
+	curl -sL https://nsolid-deb.nodesource.com/nsolid_setup_4.x > $@
 	chmod ug+x $@
 
 sources.list: node-source-setup.sh
 	bash ./$<
 
 install: sources.list
-	apt-get update && apt-get install nodejs
+	apt-get update && apt-get -y install nsolid-dubnium nsolid-console
+
+uninstall:
+	apt-get purge nodejs
+
+clean:
+	rm node-source-setup.sh
