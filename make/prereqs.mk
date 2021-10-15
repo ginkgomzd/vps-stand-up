f2d988066894afbf4d74da0b4d99fe9479144858
ginkgoMZD <mzd@ginkgostreet.com>
2021-10-14 20:35:04 -0400

    refactor prereqs.mk;

--- make/prereqs.mk
+++ make/prereqs.mk
@@ -1,32 +1,26 @@
 
-this-dir := $(dir $(lastword $(MAKEFILE_LIST)))
-include $(this-dir)/inc.functions.mk
+PACKAGES ?= wget git debconf-utils zip unzip curl bash-completion
 
-all: wget git debconf-utils repo.certbot prereqs.keyscan
+default: prereqs.packages prereqs.keyscan
 
-.PHONY: wget
-wget:
-	$(call install-package, wget)
+define install-pkg
+	dpkg -s $1 >/dev/null || sudo -E apt-get -y install $1
 
-.PHONY: git
-git:
-	$(call install-package, git)
+endef
 
-.PHONY: debconf-utils
-debconf-utils:
-	$(call install-package, debconf-utils)
+prereqs.packages: export DEBIAN_FRONTEND ?= noninteractive
+prereqs.packages:
+	$(foreach pkg,${PACKAGES},$(call install-pgk,${pkg}))
+	touch $@
 
-repo.certbot:
-	# Add the repo for CertBot/Let's Encrypt
-	sudo add-apt-repository -y ppa:certbot/certbot
-	touch repo.certbot
+KEYSCAN_HOSTS ?= github.com
 
-.PHONY: unzip
-unzip:
-	$(call install-package, unzip)
+define keyscan
+	ssh-keyscan -H $1 >> ~/.ssh/known_hosts
+
+endef
 
 prereqs.keyscan:
 	test -d ~/.ssh || mkdir ~/.ssh
-	ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-	ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts
-	touch $(@)
+	$(foreach host,${KEYSCAN_HOSTS}, $(call keyscan, ${host}))
+	touch $@
