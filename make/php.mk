394d13b15cddc3629c3df43988a725983344d4fb
ginkgoMZD <mzd@ginkgostreet.com>
2021-10-21 19:34:06 -0400

    additional php extensions (mostly civi requirements);

--- make/php.mk
+++ make/php.mk
@@ -14,5 +14,7 @@ apache-module:
 	apt update
 	apt install -y php8.0 libapache2-mod-php8.0
 
+PHP80_EXTENSIONS ?= php8.0-mysql php8.0-gd php8.0-curl php8.0-bcmath php8.0-mbstring php8.0-zip php8.0-intl php8.0-xml
+
 extensions:
-	apt install -y php8.0-mysql php8.0-gd
+	apt install -y ${PHP80_EXTENSIONS}
