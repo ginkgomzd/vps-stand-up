fb67c8b048fabdcd155600492e929a10c02af791
ginkgoMZD <mzd@ginkgostreet.com>
2022-03-18 21:17:26 -0400

    mv postfix;

--- make/conf.server.postfix.mk
+++ make/conf.server.postfix.mk
@@ -1,16 +1,4 @@
 
-this-dir := $(dir $(lastword $(MAKEFILE_LIST)))
-include $(this-dir)/inc.functions.mk
-
-REPLACE_CMD := $(this-dir)../bin/replace_file
-PATCH_FILE_CMD := $(this-dir)../bin/patch_file
-
-all: conf.server.postfix conf.server.dkim
-
-conf.server.mysql:
-	$(MAKE) -f $(this-dir)conf.server.mysql.mk
-	@ touch $(@)
-
 postfix/protocols := postfix	postfix/protocols	select	all
 postfix/root_address := postfix	postfix/root_address	string	clients@ginkgostreet.com
 postfix/procmail := postfix	postfix/procmail	boolean	false
@@ -26,9 +14,18 @@ postfix/mailname := postfix	postfix/mailname	string	$(STANDUP_FQDN)
 postfix/main_cf_conversion_warning := postfix	postfix/main_cf_conversion_warning	boolean	true
 
 conf.server.postfix:
-	$(MAKE) -f make/conf.server.postfix.mk
-	@ touch $(@)
-
-conf.server.dkim: conf.server.postfix
-	$(MAKE) -f make/conf.server.dkim.mk
+	$(call debconf-set-selection,$(postfix/protocols))
+	$(call debconf-set-selection,$(postfix/root_address))
+	$(call debconf-set-selection,$(postfix/procmail))
+	$(call debconf-set-selection,$(postfix/mailbox_limit))
+	$(call debconf-set-selection,$(postfix/recipient_delim))
+	$(call debconf-set-selection,$(postfix/chattr))
+	$(call debconf-set-selection,$(postfix/compat_conversion_warning))
+	$(call debconf-set-selection,$(postfix/rfc1035_violation))
+	$(call debconf-set-selection,$(postfix/destinations))
+	$(call debconf-set-selection,$(postfix/mynetworks))
+	$(call debconf-set-selection,$(postfix/main_mailer_type))
+	$(call debconf-set-selection,$(postfix/mailname))
+	$(call debconf-set-selection,$(postfix/main_cf_conversion_warning))
+	dpkg-reconfigure -f noninteractive postfix
 	@ touch $(@)
